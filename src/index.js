fd4c581f90219f0bc8f2699fe3d01bce72e2d804
Michael Okello <okellofrost@gmail.com>
2022-07-26 22:27:09 +0300

    react intro

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Teacher } from './Teacher';
+import Teacher , {promote} from './teacher';
 import { Person } from './Person';
+
 const element = <h1>Hello World</h1>;
 //console.log(element);
 ReactDOM.render(element,document.getElementById('root'));
@@ -9,5 +10,5 @@ ReactDOM.render(element,document.getElementById('root'));
 const person = new Person('Michael');
 console.log(person);
 
-const techer = new Teacher('MIke', 'MSc');
-console.log(techer);
+const teacher = new Teacher("Mike", "BSc IT");
+ teacher.teach();
--- src/teacher.js
+++ src/teacher.js
@@ -1,12 +1,13 @@
 import { Person } from "./Person";
 
-export class Teacher extends Person{
+export  function promote(){}
+export default class Teacher extends Person{
     constructor(name, degree){
         super(name);
         this.degree = degree;
     }
     teach(){
-        console.log("Teach");
+        console.log("Teach Me Mosh");
     }
 }
 
